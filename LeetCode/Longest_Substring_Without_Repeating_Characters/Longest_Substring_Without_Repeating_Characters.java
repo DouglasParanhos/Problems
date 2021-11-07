100f485b29bac6ec18a9833296b4fc3b11747d13
Paranhos <douglasfaparanhos@gmail.com>
2021-11-07 22:46:36 +0000

    Good solution but can be improved

--- LeetCode/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.java
+++ LeetCode/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.java
@@ -1,26 +1,32 @@
 import java.util.HashSet;
 
 class Solution {
+    
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> letters = new HashSet<Character>();
+        Map<Character, Integer> lettersPositions = new HashMap<Character, Integer>();
         int longestSequence = 0;
         
-        for(int i = 0; i < s. length(); i++){
+        for(int i = 0; i < s.length(); i++){
 
             Character letter = new Character(s.charAt(i));
             
-            if(letters.contains(letter)){
-                
-                if(letters.size() > longestSequence){
-                    longestSequence = letters.size();
-                }
-                
-                letters.clear();
-            }
+            if (lettersPositions.get(letter) != null) lettersPositions = remove(lettersPositions, letter);
             
-            letters.add(letter);
+            lettersPositions.put(letter, i);
+            
+            if(lettersPositions.keySet().size() > longestSequence) longestSequence = lettersPositions.keySet().size();
         }
         
-        return letters.size() > longestSequence ? letters.size() : longestSequence;
+        return longestSequence;
+    }
+    
+    private Map<Character, Integer> remove(Map<Character, Integer> lettersPositions, Character letter){
+        Integer positionWanted = lettersPositions.get(letter);
+        
+        lettersPositions = lettersPositions.entrySet().stream()
+            .filter(entry -> entry.getValue() > positionWanted)
+            .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
+        
+        return lettersPositions;
     }
 }
