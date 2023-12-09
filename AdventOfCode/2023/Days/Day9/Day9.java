4f2be41a328e88d46b8d20c8490f9b828bc81f24
Douglas Paranhos <douglasfaparanhos@gmail.com>
2023-12-09 11:49:05 +0000

    Day 9 part 1 working.

---
+++ AdventOfCode/2023/Days/Day9/Day9.java
@@ -0,0 +1,45 @@
+package Days.Day9;
+
+import Days.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day9 extends Day {
+
+    public Day9() {
+        super(9);
+    }
+
+    @Override
+    public Number execPart1() {
+        long sum = 0;
+        for(String s: this.inputLines) {
+            sum += getNextNumber(s);
+        }
+        return sum;
+    }
+
+    private long getNextNumber(String line) {
+        return getNextNumberFromList(Stream.of(line.split(" ")).map(Long::parseLong).collect(Collectors.toList()));
+    }
+
+    private Long getNextNumberFromList(List<Long> list) {
+        if(list.stream().distinct().count() <= 1) {
+            return list.get(0);
+        }
+
+        List<Long> newList = new ArrayList<>();
+        for(int i=1; i < list.size(); i++) {
+            newList.add(list.get(i) - list.get(i-1));
+        }
+        return list.get(list.size() - 1) + getNextNumberFromList(newList);
+    }
+
+    @Override
+    public Number execPart2() {
+        return null;
+    }
+}
--- AdventOfCode/2023/Main.java
+++ AdventOfCode/2023/Main.java
@@ -6,10 +6,11 @@ import Days.Day5.Day5;
 import Days.Day6.Day6;
 import Days.Day7.Day7;
 import Days.Day8.Day8;
+import Days.Day9.Day9;
 
 public class Main {
 
     public static void main(String... args) {
-        System.out.println(new Day8().execPart2());
+        System.out.println(new Day9().execPart1());
     }
 }
