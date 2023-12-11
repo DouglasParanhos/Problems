529ea842b833979c4c113bf58ec4a78275a038ec
Douglas Paranhos <douglasfaparanhos@gmail.com>
2023-12-11 23:02:06 +0000

    Day 11 part 1 working.

---
+++ AdventOfCode/2023/Days/Day11/Day11.java
@@ -0,0 +1,92 @@
+package Days.Day11;
+
+import Days.Day;
+
+import java.util.*;
+import java.util.List;
+
+public class Day11 extends Day {
+
+    private final String[][] matrix;
+    Set<Integer> duplicatedRows = new HashSet<>();
+    Set<Integer> duplicatedColumns = new HashSet<>();
+
+    public Day11() {
+        super(11);
+        int x = this.inputLines.size();
+        int y = this.inputLines.get(0).split("").length;
+        matrix = new String[x][y];
+
+        List<List<String>> temp = new ArrayList<>();
+        this.inputLines.forEach(line -> temp.add(Arrays.stream(line.split("")).toList()));
+
+        for(int i = 0; i < temp.size(); i++) {
+            if(temp.get(i).stream().allMatch(point -> point.equals("."))) {
+                duplicatedRows.add(i);
+            }
+            for(int j = 0; j < temp.get(0).size(); j++) {
+                matrix[i][j] = temp.get(i).get(j);
+            }
+        }
+
+
+        for(int j = 0; j < temp.get(0).size(); j++) {
+            boolean columnAllSame = true;
+            for(int i = 0; i < temp.get(0).size(); i++) {
+                if (!temp.get(i).get(j).equals(".")) {
+                    columnAllSame = false;
+                    break;
+                }
+            }
+            if(columnAllSame) {
+                duplicatedColumns.add(j);
+            }
+        }
+    }
+
+    @Override
+    public Number execPart1() {
+        List<Point> galaxies = new ArrayList<>();
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j].equals("#")) {
+                    galaxies.add(new Point(i,j));
+                }
+            }
+        }
+
+        int sum = 0;
+        for(int i = 0; i < galaxies.size() - 1; i++) {
+            sum += calculateAllDistancesGalaxy(galaxies.get(i), galaxies.subList(i + 1, galaxies.size()));
+        }
+
+        return sum;
+    }
+
+    private int calculateAllDistancesGalaxy(Point p, List<Point> galaxies) {
+        int distance = 0;
+        for(Point other: galaxies) {
+            distance += Math.abs(other.x - p.x) + Math.abs(other.y - p.y);
+            distance += (int) duplicatedRows.stream().filter(row -> row < Math.max(other.x, p.x) && row > Math.min(other.x, p.x)).count();
+            distance += (int) duplicatedColumns.stream().filter(row -> row < Math.max(other.y, p.y) && row > Math.min(other.y, p.y)).count();
+        }
+
+        return distance;
+    }
+
+    @Override
+    public Number execPart2() {
+        return null;
+    }
+
+    private static class Point {
+        int x, y;
+
+        // List<Integer> distances = new ArrayList<>();
+        private Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
--- AdventOfCode/2023/Main.java
+++ AdventOfCode/2023/Main.java
@@ -1,5 +1,6 @@
 import Days.Day1.Day1;
 import Days.Day10.Day10;
+import Days.Day11.Day11;
 import Days.Day2.Day2;
 import Days.Day3.Day3;
 import Days.Day4.Day4;
@@ -12,6 +13,6 @@ import Days.Day9.Day9;
 public class Main {
 
     public static void main(String... args) {
-        System.out.println(new Day10().execPart1());
+        System.out.println(new Day11().execPart1());
     }
 }
